37fa4197dbda1dcdd2eaae2c814767403fa34420
pbukuras123 <59669536+pbukuras123@users.noreply.github.com>
2022-05-19 16:35:27 -0400

    update show page for users, allow tracking of friends stocks

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ get 'my_portfolio', to: 'users#my_portfolio'
 get 'search_stock', to: 'stocks#search'
 get 'friends', to: 'users#my_friends'
 get 'search_friend', to: 'users#search'
+resources :users, only: [:show]
 
 end

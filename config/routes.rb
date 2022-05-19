0405ab78bd07382b4af4bf1a4d9c914999852f48
pbukuras123 <59669536+pbukuras123@users.noreply.github.com>
2022-05-19 16:09:02 -0400

    add friendships, stop following route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
   resources :user_stocks, only: [:create, :destroy]
+  resources :friendships, only: [:create, :destroy]
   devise_for :users
 root 'welcome#index'
 get 'my_portfolio', to: 'users#my_portfolio'
 get 'search_stock', to: 'stocks#search'
+get 'friends', to: 'users#my_friends'
+get 'search_friend', to: 'users#search'
 
 end

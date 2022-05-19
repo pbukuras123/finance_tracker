d1c1f97934c93ee622c3ec732b4aa20f96c7b2ba
pbukuras123 <59669536+pbukuras123@users.noreply.github.com>
2022-05-19 16:13:55 -0400

    add following friend capability

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,13 @@
 class FriendshipsController < ApplicationController
   def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "following friend"
+    else
+      flash[:alert] = "There was something wrong with the tracking request"
+    end
+    redirect_to friends_path
   end
 
   def destroy
